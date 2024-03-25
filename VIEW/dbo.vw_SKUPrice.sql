b4294f9b2139de9e001e75b6ada4685370c7e7d2
Dmitriy Tereshonkov <146711271+d-tereshonkov@users.noreply.github.com>
2024-03-25 10:04:11 +0300

    Update dbo.vw_SKUPrice.sql

--- VIEW/dbo.vw_SKUPrice.sql
+++ VIEW/dbo.vw_SKUPrice.sql
@@ -1,13 +1,13 @@
 /*
 Создать представление dbo.vw_SKUPrice:
-	- Возвращает все атрибуты продуктов из таблицы dbo.SKU 
-      и расчетный атрибут со стоимостью одного продукта (используя функцию dbo.udf_GetSKUPrice)
+Возвращает все атрибуты продуктов из таблицы dbo.SKU 
+и расчетный атрибут со стоимостью одного продукта (используя функцию dbo.udf_GetSKUPrice)
 */
 create view dbo.vw_SKUPrice
 as
 select 
-	ID
-	,Code
-	,Name
-	,udf_GetSKUPrice(ID) as price
+    ID
+    ,Code
+    ,Name
+    ,udf_GetSKUPrice(ID) as PriceSku
 from dbo.SKU
