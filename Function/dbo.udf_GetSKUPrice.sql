c8a41bb31d530abca87d5014b2db9d6c39f713fc
Dmitriy Tereshonkov <146711271+d-tereshonkov@users.noreply.github.com>
2024-03-25 09:19:21 +0300

    Update dbo.udf_GetSKUPrice.sql

--- Function/dbo.udf_GetSKUPrice.sql
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -1,18 +1,18 @@
-/*
-Создать функцию dbo.udf_GetSKUPrice:
-	- Входной параметр @ID_SKU
-	- Рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
-	  сумма Value по переданному SKU / сумма Quantity по переданному SKU
-	- На выходе значение типа decimal(18, 2)
-*/
+-- Создать функцию dbo.udf_GetSKUPrice
 create function dbo.udf_GetSKUPrice(
+   -- Входной параметр @ID_SKU
    ID_SKU int
 )
+-- На выходе значение типа decimal(18, 2)	
 returns decimal(18, 2)
 
 begin
-	declare sku_price DECIMAL(18, 2);
+    declare sku_price DECIMAL(18, 2);
 
+    /*
+    Рассчитаем стоимость передаваемого продукта из таблицы dbo.Basket по формуле
+    сумма Value по переданному SKU / сумма Quantity по переданному SKU
+    */
     select sum(Value / Quantity) as price
     into sku_price
     from dbo.Basket
