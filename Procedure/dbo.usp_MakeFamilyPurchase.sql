c526612c45b9c4a51eee4f86cfd498eda0825c96
Dmitriy Tereshonkov <146711271+d-tereshonkov@users.noreply.github.com>
2024-03-25 09:36:46 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- Procedure/dbo.usp_MakeFamilyPurchase.sql
+++ Procedure/dbo.usp_MakeFamilyPurchase.sql
@@ -1,41 +1,40 @@
 /*
-Создать процедуру dbo.usp_MakeFamilyPurchase:
-	- Входной параметр (@FamilySurName varchar(255)) одно из значений атрибута SurName таблицы dbo.Family
-	- Процедура при вызове обновляет данные в таблицы dbo.Family в поле BudgetValue по логике
-	  dbo.Family.BudgetValue - sum(dbo.Basket.Value), где dbo.Basket.Value покупки для переданной в процедуру семьи
-	- При передаче несуществующего dbo.Family.SurName пользователю выдается ошибка, что такой семьи нет
-
+Создадим процедуру, которая рассчитает и обновит бюджет семьи
+Процедура при вызове обновляет данные в таблицы dbo.Family в поле BudgetValue по логике
+dbo.Family.BudgetValue - sum(dbo.Basket.Value), где dbo.Basket.Value покупки для переданной в процедуру семьи
 */
--- Создадим процедуру, которая рассчитает и обновит бюджет семьи
 create procedure dbo.usp_MakeFamilyPurchase(
+-- Входной параметр (@FamilySurName varchar(255)) одно из значений атрибута SurName таблицы dbo.Family
 in FamilySurName varchar(100)
 )
 begin	
-	-- Задаем переменные в которые будем помещать значения
-	declare family_id int;
-	declare	total_basket_value decimal(18, 2);
+    -- Задаем переменные в которые будем помещать значения
+    declare FamilyId int;
+    declare TotalBasketValue decimal(18, 2);
     
-   	-- Находим ID семьи по фамилии
+    -- Находим ID семьи по фамилии
     select ID 
-    into family_id
+    into FamilyId
     from dbo.Family
     where SurName = FamilySurName;
     
-    -- Проверяем, существует ли такая семья
-    if family_id is null then
-       signal sqlstate '45000' 
-       set message_text = 'Такой семьи нет';
+    /* Проверяем, существует ли такая семья
+       При передаче несуществующего dbo.Family.SurName пользователю выдается ошибка, что такой семьи нет
+    */
+    if FamilyId is null then
+        signal sqlstate '45000' 
+        set message_text = 'Такой семьи нет';
     else
         -- Общая стоимость покупок для семьи
         select sum(Value)
-        into total_basket_value
+        into TotalBasketValue
         from dbo.Basket
-        where ID_Family = family_id;
+        where ID_Family = FamilyId;
         
         -- Обновляем бюджет семьи
         update dbo.Family
-        set BudgetValue = BudgetValue - total_basket_value
-        where ID = family_id;
+        set BudgetValue = BudgetValue - TotalBasketValue
+        where ID = FamilyId;
 
     end if;
 end
